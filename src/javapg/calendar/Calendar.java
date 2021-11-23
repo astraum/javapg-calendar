94b3f462e65967feaf8b7fc0804dac5a1d32077f
Seung Wan Jeong <sjeong4776@gmail.com>
2021-11-23 18:38:29 +0900

    print calendar

--- src/javapg/calendar/Calendar.java
+++ src/javapg/calendar/Calendar.java
@@ -1,7 +1,21 @@
 package javapg.calendar;
 
 public class Calendar {
+
+    public static int a = 56;
     public static void main(String[] args) {
-        System.out.println("Hello, Calendar");
+        System.out.println(" 일 월  화 수  목 금  토");
+        System.out.println("--------------------");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j <= 7; j++) {
+                int day = i * 7 + j;
+                if (day < 10) {
+                    System.out.print(" " + day + " ");
+                } else {
+                    System.out.print(day + " ");
+                }
+            }
+            System.out.print("\n");
+        }
     }
 }
