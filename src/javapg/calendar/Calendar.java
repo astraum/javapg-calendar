364e64296a023786e200b5b9b0263d954fede7ea
Seung Wan Jeong <sjeong4776@gmail.com>
2021-11-23 23:03:43 +0900

    Updated Calendar

--- src/javapg/calendar/Calendar.java
+++ src/javapg/calendar/Calendar.java
@@ -1,9 +1,15 @@
 package javapg.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
-    public static int a = 56;
-    public static void main(String[] args) {
+    private static final int[] lastDaysOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static int lastDayOfMonth(int month) {
+        return lastDaysOfMonth[month-1];
+    }
+
+    public static void printCalendar() {
         System.out.println(" 일 월  화 수  목 금  토");
         System.out.println("--------------------");
         for (int i = 0; i < 4; i++) {
@@ -18,4 +24,24 @@ public class Calendar {
             System.out.print("\n");
         }
     }
+
+    public static int a = 56;
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("반복횟수를 입력하세요.");
+        int repeat = scanner.nextInt();
+
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("달을 입력하세요.");
+            int month = scanner.nextInt();
+            int lastDay = lastDayOfMonth(month);
+
+            System.out.printf("%d월은 %d일까지 있습니다.\n", month, lastDay);
+
+        }
+        System.out.println("프로그램을 종료합니다.");
+        scanner.close();
+    }
+
 }
